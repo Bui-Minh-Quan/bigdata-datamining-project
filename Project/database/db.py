b26ddab776d231acfece1017b3bd87792b6b40f3
Bui-Minh-Quan <buiminhquandz2005@gmail.com>
2025-11-26 21:40:22 +0700

    feat: Enable MongoDB connection URL configuration via environment variable

--- Project/database/db.py
+++ Project/database/db.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
-
+from dotenv import load_dotenv
+import os
 def get_mongo_client():
     # Local MongoDB mặc định chạy ở port 27017
-    mongo_url = "mongodb://localhost:27017"
+    mongo_url = os.getenv("MONGO_URL", "mongodb://localhost:27017")
     
     client = MongoClient(mongo_url)
+    print(f"Connected to MongoDB at {mongo_url}")
     return client
 
 def get_database(db_name="bigdata_trr"):
